029439c0e8d62a9d866f78a42f43b09245b61ab4
Daniel Zwolenski <zonski@gmail.com>
2012-11-25 08:52:00 +1100

    Added Mojo to generate development key store

--- src/main/java/com/zenjava/javafx/maven/plugin/GenerateKeyStoreMojo.java
+++ src/main/java/com/zenjava/javafx/maven/plugin/GenerateKeyStoreMojo.java
@@ -28,7 +28,7 @@ import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
  * @phase validate
  * @requiresDependencyResolution
  */
-public class GenerateKeystoreMojoOld extends AbstractBundleMojo {
+public class GenerateKeyStoreMojo extends AbstractBundleMojo {
 
     /**
      * @parameter default-value="false" expression="${overwriteKeyStore}"
