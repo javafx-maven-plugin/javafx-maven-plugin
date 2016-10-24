48049662ee9f8d89cc15233e99cf63100911287b
FibreFoX <fibrefox@dynamicfiles.de>
2016-10-24 23:24:18 +0200

    added testing project for new additionalBundlerResources property

---
+++ src/it/28-additionalBundlerResources/src/main/java/com/zenjava/test/Main.java
@@ -0,0 +1,15 @@
+package com.zenjava.test;
+
+import javafx.stage.Stage;
+
+public class Main extends javafx.application.Application {
+
+    public static void main(String[] args){
+        Main.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.exit(0);
+    }
+}
