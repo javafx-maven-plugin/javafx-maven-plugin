f403ff9fb4729e408705de296962e6daaada6817
Florian Brunner <fbrunnerreg@gmx.ch>
2017-02-02 23:03:37 +0100

    #137 Provide packaging support - integration test

--- src/it/29-property-names/src/main/java/com/zenjava/test/Main.java
+++ src/it/29-property-names/src/main/java/com/zenjava/test/Main.java
@@ -1,16 +1,21 @@
 package com.zenjava.test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        LOGGER.log(Level.INFO, "Starting JavaFX application...");
         primaryStage.setScene(new Scene(new Label("Hello World!")));
         primaryStage.show();
+        LOGGER.log(Level.INFO, "Started JavaFX application!");
     }
 
     public static void main(String[] args) {
