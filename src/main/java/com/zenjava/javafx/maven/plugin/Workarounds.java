ea14b71c046b9e8db652bfe47259fb9e17529083
FibreFoX <fibrefox@dynamicfiles.de>
2018-01-16 00:16:08 +0100

    removed MacAppBundlerWithAdditionalResources bundler, as this is not compatible with JDK 9 anymore

--- src/main/java/com/zenjava/javafx/maven/plugin/Workarounds.java
+++ src/main/java/com/zenjava/javafx/maven/plugin/Workarounds.java
@@ -349,26 +349,8 @@ public class Workarounds {
     }
 
     public Bundler applyWorkaroundForNativeMacBundler(final Bundler b, String currentRunningBundlerID, Map<String, Object> params, File additionalBundlerResources) {
-        String workaroundForNativeMacBundlerDoneMarker = "WorkaroundForNativeMacBundler.done";
-        if( "mac.app".equals(currentRunningBundlerID) && !params.containsKey(workaroundForNativeMacBundlerDoneMarker) ){
-            // 1) replace current running bundler with our own implementation
-            Bundler specialMacBundler = new MacAppBundlerWithAdditionalResources();
-
-            // 2) replace other bundlers using mac.app-bundler inside
-            getLog().info("Setting replacement of the 'mac.app'-bundler.");
-            params.put("mac.app.bundler", specialMacBundler);
-            params.put(workaroundForNativeMacBundlerDoneMarker, true);
-            Path specificFolder = additionalBundlerResources.toPath().resolve("mac.app");
-            // check if there is a special folder, otherwise put all stuff here
-            if( Files.exists(specificFolder) && Files.isDirectory(specificFolder) ){
-                getLog().info("Using special 'mac.app' bundler-folder.");
-                params.put(MacAppBundlerWithAdditionalResources.ADDITIONAL_BUNDLER_RESOURCES.getID(), specificFolder.toAbsolutePath().toFile());
-            } else {
-                params.put(MacAppBundlerWithAdditionalResources.ADDITIONAL_BUNDLER_RESOURCES.getID(), additionalBundlerResources);
-            }
-
-            return specialMacBundler;
-        }
+        getLog().info("Workaround for native mac bundler not present in this version (due to JDK9 compatibility conflicts).");
+        getLog().info("Please use some older version of this plugin.");
         return b;
     }
 
