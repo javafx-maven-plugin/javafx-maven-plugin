c14a14a68c87433ca3bef7c1a3d6024ee87e1db3
theanuradha <theanuradha@gmail.com>
2019-11-27 12:27:50 +0000

    added support to config Main jar and classpath

--- src/main/java/com/zenjava/javafx/maven/plugin/NativeMojo.java
+++ src/main/java/com/zenjava/javafx/maven/plugin/NativeMojo.java
@@ -498,6 +498,8 @@ public class NativeMojo extends AbstractJfxToolsMojo {
             params.put(StandardBundlerParam.SHORTCUT_HINT.getID(), needShortcut);
             params.put(StandardBundlerParam.MENU_HINT.getID(), needMenu);
             params.put(StandardBundlerParam.MAIN_CLASS.getID(), mainClass);
+            params.put(StandardBundlerParam.MAIN_JAR.getID(), mainJar);
+            params.put(StandardBundlerParam.CLASSPATH.getID(), classpath);
 
             Optional.ofNullable(jvmProperties).ifPresent(jvmProps -> {
                 params.put(StandardBundlerParam.JVM_PROPERTIES.getID(), new HashMap<>(jvmProps));
